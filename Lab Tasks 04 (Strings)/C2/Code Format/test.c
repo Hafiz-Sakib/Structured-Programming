fe815d681ca5c59826ef9b4852a4fd84a525aa2f
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-03-05 22:00:22 +0600

    update

--- Lab Tasks 04 (Strings)/C2/Code Format/test.c
+++ Lab Tasks 04 (Strings)/C2/Code Format/test.c
@@ -1,45 +1,20 @@
 #include <stdio.h>
+#include <math.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <float.h>
+#include <limits.h>
+#include <time.h>
+#define ll long long
+#define scan(data) scanf("%d", &data)
+#define print(data) printf("%d\n", data)
+#define fscan(data) scanf("%f", &data)
+#define fprint(data) printf("%.2f\n", data)
 
 int main()
 {
-    char string[100], replace_char, new_char;
-    int i, first_occurrence = -1, last_occurrence = -1;
-
-    printf("Enter a string: ");
-    fgets(string, sizeof(string), stdin);
-
-    printf("Enter a character to replace: ");
-    scanf("%c", &replace_char);
-
-    printf("Enter the new character: ");
-    scanf("%c", &new_char);
-
-    // Find the index of the first and last occurrence of the character to replace
-    for (i = 0; string[i] != '\0'; i++)
-    {
-        if (string[i] == replace_char)
-        {
-            if (first_occurrence == -1)
-            {
-                first_occurrence = i;
-            }
-            last_occurrence = i;
-        }
-    }
-
-    // Replace the first occurrence of the character
-    if (first_occurrence != -1)
-    {
-        string[first_occurrence] = new_char;
-    }
-
-    // Replace the last occurrence of the character
-    if (last_occurrence != -1 && last_occurrence != first_occurrence)
-    {
-        string[last_occurrence] = new_char;
-    }
-
-    printf("Modified string: %s", string);
 
     return 0;
 }
