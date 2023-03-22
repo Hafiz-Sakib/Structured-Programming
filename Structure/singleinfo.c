578c65b87856d04b9ee08ce6d7022446079f216d
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-03-23 00:22:00 +0600

    Single Information of a Student

--- Structure/singleinfo.c
+++ Structure/singleinfo.c
@@ -69,7 +69,7 @@ int main()
     ReadStudents(n, student);
 
     int roll = 0;
-    printf("Enter The Student ID that you Searching for : \n");
+    printf("\nEnter The Student ID that you Searching for : \n");
     scanf("%d", &roll);
     for (int i = 0; i < n; i++)
     {
