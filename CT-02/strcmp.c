789f36a509c85d6047a184efb985a5b7f83e888c
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-03-22 21:36:46 +0600

    update

---
+++ CT-02/strcmp.c
@@ -0,0 +1,14 @@
+/*
+Name : Hafiz Sakib
+Id : 0222210005101118
+
+*/
+
+// Problem : Write a C Program that works like strcmp function!
+
+#include <stdio.h>
+int main()
+{
+
+    return 0;
+}
---
+++ codechef/d.cpp
@@ -0,0 +1,120 @@
+// Bismillahir Rahmanir Rahim
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long ull;
+
+typedef vector<ll> vi;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<vector<ll>> vvi;
+typedef vector<pair<ll, ll>> vpii;
+typedef vector<pair<string, ll>> vpsi;
+typedef pair<ll, ll> pii;
+typedef map<ll, ll> mii;
+typedef map<string, ll> msi;
+typedef set<ll> si;
+typedef set<string> ss;
+
+#define pb push_back
+#define mp make_pair
+#define in insert
+#define fi first
+#define se second
+#define space ' '
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+#define b() begin()
+#define e() end()
+#define all(data) data.begin(), data.end()
+#define rall(data) data.rbegin(), data.rend()
+#define vecMax(data) *max_element(data.begin(), data.end())
+#define vecMin(data) *min_element(data.begin(), data.end())
+#define vecSum(data) accumulate(data.begin(), data.end(), 0)
+
+#define stringLower(data) transform(data.begin(), data.end(), data.begin(), ::tolower)
+#define stringUpper(data) transform(data.begin(), data.end(), data.begin(), ::toupper)
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void Boom()
+{
+    int n;
+    cin >> n;
+    vi p(n), q(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> p[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> q[i];
+    }
+    if (p[0] != q[0])
+    {
+        no;
+        return;
+    }
+    if (p[n - 1] != q[n - 1])
+    {
+        no;
+        return;
+    }
+    int a = 0, b = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (p[i] == 0)
+        {
+            a++;
+        }
+        else
+        {
+            b++;
+        }
+    }
+    for (int i = 1; i < n - 1; i++)
+    {
+        if (p[i] != q[i] && p[i] == 0 && b == 0)
+        {
+            no;
+            return;
+        }
+        if (p[i] != q[i] && p[i] == 1)
+        {
+            no;
+            return;
+        }
+    }
+    yes;
+}
+
+int main()
+{
+    Boost;
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        Boom();
+    }
+
+    return 0;
+}
