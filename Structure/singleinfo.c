a67e166e9ed27f9e0e6b8ee08b42b25bd7e970cc
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-03-23 00:16:13 +0600

    Single Information of a Student

---
+++ Structure/singleinfo.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+
+struct students
+{
+    char name[20];
+    int std_id;
+    float cgpa;
+    char uni_name[20];
+    char dept[10];
+};
+
+void ReadStudents(int n, struct students *student)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("\nEnter information of Student %d : \n\n", (i + 1));
+
+        printf("Enter the name of Student : \n");
+        fflush(stdin);
+        gets(student[i].name);
+
+        printf("Enter the ID of Student : \n");
+        scanf("%d", &student[i].std_id);
+
+        printf("Enter the CGPA of Student : \n");
+        scanf("%f", &student[i].cgpa);
+
+        printf("Enter the University Name of the Student : \n");
+        scanf("%d", &student[i].uni_name);
+
+        printf("Enter the Department Name of the Student : \n");
+        scanf("%d", &student[i].uni_name);
+    }
+}
+
+void WriteStudents(struct students student, int n)
+{
+    if (n == student.std_id)
+    {
+        printf("\nThis is the Information About : %s\n\n", student.name);
+
+        printf("Name of Student is : %s\n", student.name);
+
+        printf("Student ID of  %s is : %d\n", student.name, student.std_id);
+
+        printf("CGPA of %s is : %.2f\n", student.name, student.cgpa);
+
+        printf("University Name of  %s is : %d\n", student.name, student.uni_name);
+
+        printf("Departmanet Name of %s is : %d\n", student.name, student.dept);
+        }
+}
+
+int main()
+{
+    int n;
+    printf("Enter the numbers of students in your class : \n");
+    scanf("%d", &n);
+    struct students student[n];
+
+    ReadStudents(n, student);
+
+    int roll = 0;
+    printf("Enter The Student ID that you Searching for : \n");
+    scanf("%d", &roll);
+    for (int i = 0; i < n; i++)
+    {
+        WriteStudents(student[i], roll);
+    }
+
+    return 0;
+}
